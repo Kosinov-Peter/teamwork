46d7161f5055b33311d8ce248c5bf74b740dc106
Kosinov-Peter <67550085+Kosinov-Peter@users.noreply.github.com>
2020-12-01 22:19:09 +0300

    second

--- сорт_выбор/sorting_by_choice/main.c
+++ сорт_выбор/sorting_by_choice/main.c
@@ -6,15 +6,15 @@ int main() {
     int kol = 0;
     scanf("%d",&kol);
     int* mas = (int*) malloc(kol*sizeof(int));
-    for (int i = 0; i < kol; i++ ) {
+    for (int i = 0; i < kol; ++i ) {
         scanf("%d",&mas[i]);
     }
 
     int min = 0;
     int dtime = 0;
-    for (int i = 0; i < kol-1; i++ ) {
+    for (int i = 0; i < kol-1; ++i ) {
         min = i;
-        for (int j = i+1; j < kol; j++ ) {
+        for (int j = i+1; j < kol; ++j ) {
             if (mas[j] < mas[min]) {
                 min = j;
             }
@@ -25,22 +25,9 @@ int main() {
     }
 
     printf("\n");
-    for (int i = 0; i < kol; i++) {
+    for (int i = 0; i < kol; ++i) {
         printf("%d ",mas[i]);
     }
     free(mas);
     return 0;
 }
-
-
-    for ( i = 1 to (kol-1) ) {
-        min = i;
-
-        for (int j = i+1; j < kol; j++ ) {
-            if (mas[j] < mas[min]) {
-                min = j;
-            }
-        }
-
-        SWAP mas[i] and mas[min];
-    }
